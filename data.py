e188f6e85271ee6168b6dddd51c65ab425edf880
codeIntrovert <9131411681hasan@gmail.com>
2023-07-30 12:00:31 +0530

    search function added, minor changes

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-
+#imageName varible only gets used for menu.html macro on search results
 
 lecturesData = [
     {
@@ -25,49 +25,70 @@ lecturesData = [
 ]
 
 reactjs = {
+    'courseName': 'ReactJS',#used on search results
     'name': 'ReactJS',
     'logo' : 'react-original',
     'difficulty': 'Intermediate',
     'duration': '1 week',
     'rating': '5.0',
     'price': 'Free',
+    'imageName': 'popular-02.jpg',
     'description': 'ReactJS is a powerful, efficient and user-friendly JavaScript library for building interactive interfaces.'
 }
 ML = {
+    'courseName': 'ML+AI',#used on search results
     'name': 'Machine Learning',
     'logo': 'python-plain',
     'difficulty' : 'Advance',
     'duration': '3 week',
     'rating': '4.9',
     'price': '$25',
+    'imageName': 'popular-02.jpg',
     'description': 'Training computers to learn from data, improving performance without explicit instructions.'
 }
 JavaScript = {
+'courseName': 'JavaScript',#used on search results
 'name': 'JavaScript Fundamentals',
 'logo': 'javascript-plain',
 'difficulty': 'Beginner',
 'duration': '4 weeks',
 'rating': '4.7',
 'price': '$20',
+'imageName': 'popular-02.jpg',
 'description': 'Learn the basics of JavaScript programming for front-end and back-end development.'
 }
 
 Java = {
+'courseName': 'Java',#used on search results
 'name': 'Java Programming',
 'logo': 'java-plain',
 'difficulty': 'Intermediate',
 'duration': '5 weeks',
 'rating': '4.8',
 'price': '$30',
+'imageName': 'popular-02.jpg',
 'description': 'Get started with Java and object-oriented programming for building robust applications.'
 }
 
 CSharp = {
+'courseName': 'C#',#used on search results
 'name': 'C# Essentials',
 'logo': 'csharp-plain',
 'difficulty': 'Beginner',
 'duration': '2 weeks',
 'rating': '4.5',
 'price': '$15',
+'imageName': 'popular-02.jpg',
 'description': 'Introduction to C# programming language and its applications in various domains.'
+}
+Django = {
+'courseName': 'Django',#used on search results
+'name': 'Django Framework',
+'logo': 'django-plain',
+'difficulty': 'Intermediate',
+'duration': '3 weeks',
+'rating': '4.6',
+'price': '$25',
+'imageName': 'popular-02.jpg',
+'description': 'Learn the basics of Django framework for building robust web applications.'
 }
--- menuData.py
+++ menuData.py
@@ -1,3 +1,4 @@
+
 beginner = [
     {
         'courseName': 'HTML',
