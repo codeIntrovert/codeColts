ce9e72b698b7baf6e63ecfce704dc51ff9b0271b
codeIntrovert <9131411681hasan@gmail.com>
2023-08-25 11:54:14 +0530

    profile route removed, minor bug fixes

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from routes.index_routes import index_blueprint
 from routes.browse_routes import browse_blueprint
 from routes.error_routes import error_blueprint
 from routes.login_routes import login_blueprint
-from routes.profile_routes import profile_blueprint
 import data, menuData
 
 app = Flask(__name__)
@@ -13,7 +12,6 @@ app.register_blueprint(index_blueprint)
 app.register_blueprint(browse_blueprint)
 app.register_blueprint(error_blueprint)
 app.register_blueprint(login_blueprint)
-app.register_blueprint(profile_blueprint)
 
 if __name__ == '__main__':
     app.run()
