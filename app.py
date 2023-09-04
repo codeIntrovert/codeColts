c31b3ba48d28be72a2f6595d1b1176c321e96830
codeIntrovert <9131411681hasan@gmail.com>
2023-09-04 11:44:01 +0530

    fixed linux compaltibility, added links in footer

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from routes.index_routes import index_blueprint
 from routes.browse_routes import browse_blueprint
-from routes.error_routes import error_blueprint
 from routes.login_routes import login_blueprint
 from routes.view_routes import view_blueprint
 
@@ -11,9 +10,14 @@ app = Flask(__name__)
 # Register the Blueprints from separate files
 app.register_blueprint(index_blueprint)
 app.register_blueprint(browse_blueprint)
-app.register_blueprint(error_blueprint)
 app.register_blueprint(login_blueprint)
 app.register_blueprint(view_blueprint)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    theme_preference = request.cookies.get('theme', 'light')  # Default to 'light' if cookie not found
+    theme_css = theme_preference + "_theme"
+    return render_template('components/error.html',theme_css=theme_css), 404
 
 if __name__ == '__main__':
     app.run()
