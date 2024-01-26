7772302d1cb1ffb99f3f9fb054be072367d50431
Hasan <9131411681hasan@gmail.com>
2024-01-26 20:19:04 +0530

    minor syntax change in theme.py

--- routes/theme.py
+++ routes/theme.py
@@ -4,5 +4,4 @@ from flask import request
 
 def theme_selection():
     theme_preference = request.cookies.get('theme', 'light')  # Default to 'light' if cookie not found
-    theme_css = theme_preference + "_theme"
-    return theme_css 
+    return theme_preference + "_theme" 
